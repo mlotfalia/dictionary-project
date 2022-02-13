0d0ec061e50ae6fcc63ece21d041f9cb9fdc86ca
mlotfalia <mahsa.lotfalia@gmail.com>
2022-02-13 18:05:52 +0330

    add dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,10 +2,18 @@ import "./Dictionary.css";
 
 import React, { useState } from "react";
 
+import axios from "axios";
+
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  function handleResponse(response) {
+    return response.data[0];
+  }
   function search(event) {
     alert(`Searching for ${keyword}`);
+    //documentation    https://dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function handleKeyword(event) {
     setKeyword(event.target.value);
