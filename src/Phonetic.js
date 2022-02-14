06109d2cc9a5cea2d1fa0914069acad63df612e3
mlotfalia <mahsa.lotfalia@gmail.com>
2022-02-14 18:08:54 +0330

    add phonetic

---
+++ src/Phonetic.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import solid from "@fortawesome/fontawesome-svg-core/import.macro";
+
+export default function Phonetic(props) {
+  const element = <FontAwesomeIcon icon="fa-solid fa-volume-high" />;
+  if (props.phonetic) {
+    return (
+      <div className="Phonetic">
+        <a href={props.phonetic.audio}>{element}</a>
+
+        {props.phonetic.text}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
--- src/Results.js
+++ src/Results.js
@@ -1,4 +1,5 @@
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 import React from "react";
 
 export default function Results(props) {
@@ -6,9 +7,14 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
-        <h4>
-          <em>{props.results.phonetic}</em>
-        </h4>
+      
+          {props.results.phonetics.map(function (phonetic, index){
+          return(<div key = {index}>
+            <Phonetic phonetic = {phonetic}/>
+          </div> );
+           } )}
+
+
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
