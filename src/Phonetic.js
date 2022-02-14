9d38f600be85a57638b66b3e62695a7937137e0c
mlotfalia <mahsa.lotfalia@gmail.com>
2022-02-14 18:50:05 +0330

    update phonetic

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "font-awesome/css/font-awesome.min.css";
 
 export default function Phonetic(props) {
-  const element = <FontAwesomeIcon icon="fa-solid fa-volume-high" />;
   if (props.phonetic) {
     return (
       <div className="Phonetic">
-        <a href={props.phonetic.audio}>{element}</a>
+        <button className="btn">
+          <i className="fa-solid fa-volume-high">
+            {" "}
+            <FontAwesomeIcon icon="fa-solid fa-volume-high" />
+          </i>
+        </button>
+        <a href={props.phonetic.audio}>Listen</a>
 
         {props.phonetic.text}
       </div>
