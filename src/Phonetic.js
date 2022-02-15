919c2178a3a4b9f801a515c12b962088515ca1a5
mlotfalia <mahsa.lotfalia@gmail.com>
2022-02-15 20:41:25 +0330

    update phonetic

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -8,7 +8,7 @@ export default function Phonetic(props) {
   if (props.phonetic) {
     return (
       <div className="Phonetic">
-        <a href={props.phonetic.audio}>
+        <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
           <FontAwesomeIcon icon={solid("volume-high")} />
         </a>
         <span className="text">{props.phonetic.text}</span>
