ca705aab62a59693d15357d29708fea7f64a08c6
mlotfalia <mahsa.lotfalia@gmail.com>
2022-02-15 20:34:57 +0330

    add image

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,19 +2,34 @@ import React, { useState } from "react";
 
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
-  function handleResponse(response) {
+  let [photos, setPhotos] = useState(null);
+  function handleResponseWord(response) {
     setResults(response.data[0]);
+  }
+  function handleResponseImage(response) {
+    setPhotos(response.data.photos);
   }
   function search() {
     //documentation    https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleResponseWord);
+    let pexelsApiKey =
+      "563492ad6f917000010000013ce24b2fbf684cdd89659b4fe2348932";
+
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+
+    axios
+      .get(pexelsApiUrl, {
+        headers: { Authorization: `Bearer ${pexelsApiKey}` },
+      })
+      .then(handleResponseImage);
   }
   function handleSubmit(event) {
     event.preventDefault();
@@ -33,13 +48,18 @@ export default function Dictionary(props) {
         <section>
           <h1>What word do you looking for?</h1>
           <form onSubmit={handleSubmit}>
-            <input type="search" onChange={handleKeywordChange} defaultValue = {props.defaultKeyword} />
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
           </form>
           <div className="hint">
             suggested words: yoga, plant, unwind, breathing ...
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
